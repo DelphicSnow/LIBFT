909777c4e5af97b5e2d7d355222bac1baeddbe69
Tomas Kajanek <tkajanek@c1r5s7.42prague.com>
2023-03-10 16:24:54 +0100

    libft updated

---
+++ ft_strtrim.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkajanek <tkajanek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 20:23:20 by tkajanek          #+#    #+#             */
+/*   Updated: 2023/03/10 16:10:46 by tkajanek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		start;
+	int		end;
+	int		len;
+	char	*trimmed;
+
+	start = 0;
+	if (!s1)
+		return (0);
+	end = ft_strlen(s1) - 1;
+	while (start <= end && ft_strchr(set, s1[start]))
+		start++;
+	while (end >= start && ft_strchr(set, s1[end]))
+		end--;
+	len = end - start + 1;
+	trimmed = (char *)malloc(sizeof(char) * (len + 1));
+	if (!trimmed)
+		return (NULL);
+	ft_memcpy(trimmed, &s1[start], len);
+	trimmed[len] = '\0';
+	return (trimmed);
+}
